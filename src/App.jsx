3768c7e0721df680c40199ec03860c1255244c79
lawielas <lawie.aminu@yahoo.com>
2023-09-07 21:21:57 +0100

    program migrated to react vite

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+
+            <button id="clear" className="ac-btn">AC</button>
+            <button id="divide" className="action-btn">/</button>
+            <br/>
+            <button id="7" className="digit-btn">7</button>
+            <button id="8" className="digit-btn">8</button>
+            <button id="9" className="digit-btn">9</button>
+            <button id="multiply" className="action-btn">X</button>
+            <br/>
+            <button id="4" className="digit-btn">4</button>
+            <button id="5" className="digit-btn">5</button>
+            <button id="6" className="digit-btn">6</button>
+            <button id="add" className="action-btn">+</button>
+            <br/>
+            <button id="1" className="digit-btn">1</button>
+            <button id="2" className="digit-btn">2</button>
+            <button id="3" className="digit-btn">3</button>
+            <button id="subtract" className="action-btn">-</button>
+            <br/>
+            <button id="zero" className="digit-btn">0</button>
+            <button id="decimal" className="digit-btn">.</button>
+            <button id="equals">=</button>
+    </div>
+  )
+}
+
+export default App
