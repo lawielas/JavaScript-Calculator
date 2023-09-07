5e17d77926da5ec33ba79610a7f55874ebbda02f
lawielas <lawie.aminu@yahoo.com>
2023-09-07 23:36:00 +0100

    clear button implemented, digit buttons in works

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,62 @@ const controlStyle = 'w-14 h-10 bg-slate-600'
 
 function App() {
 
-  const [ display, setDisplay ] = useState(0)
+  const [ display, setDisplay ] = useState([''])
+  const [ inputDisplay, setInputDisplay ] = useState([0])
+
+  
+
+  const handleClear = () => {
+    setDisplay([''])
+    setInputDisplay([0])
+  }
+
+  const handleDivide = (e) => {
+    setDisplay([...display, e.target.innerText])
+    setInputDisplay([e.target.innerText])
+  }
+
+  const handleDigitClick = (e) => {
+    // if(inputDisplay.length > 0 || inputDisplay !== 0){
+    //   setInputDisplay([0])
+    // }
+
+    if(inputDisplay[0] === 0){
+      setDisplay([''])
+      setInputDisplay([e.target.innerText])
+    } else {
+      setDisplay([...display, e.target.innerText])
+      setInputDisplay([...inputDisplay, e.target.innerText])
+    }
+  }
 
   return (
     <div className=' flex flex-col justify-center items-center h-screen'>
       <div className='bg-lime-50 flex flex-col justify-center items-center p-5'>
-        <div id='display' className='bg-slate-700 w-full h-14 text-xl float-right text-white text-right p-3'>{display}</div>
+        <div className='bg-slate-700 w-full h-16 text-white text-right py-1 px-3'>
+          <p className='text-base h-5'>{display}</p>
+          <p  id='display' className='text-xl h-5'>{inputDisplay}</p>  
+        </div>
         <div className='text-lg text-white'>
-          <button id="clear" className="w-48 h-10 bg-red-500">AC</button>
-          <button id="divide" className={controlStyle}>/</button>
+          <button id="clear" className="w-48 h-10 bg-red-500" onClick={handleClear}>AC</button>
+          <button id="divide" className={controlStyle} onClick={handleDivide}>/</button>
           <br/>
-          <button id="seven" className={digitStyle}>7</button>
-          <button id="eight" className={digitStyle}>8</button>
-          <button id="nine" className={digitStyle}>9</button>
+          <button id="seven" className={digitStyle} onClick={handleDigitClick}>7</button>
+          <button id="eight" className={digitStyle} onClick={handleDigitClick}>8</button>
+          <button id="nine" className={digitStyle} onClick={handleDigitClick}>9</button>
           <button id="multiply" className={controlStyle}>X</button>
           <br/>
-          <button id="four" className={digitStyle}>4</button>
-          <button id="five" className={digitStyle}>5</button>
-          <button id="six" className={digitStyle}>6</button>
+          <button id="four" className={digitStyle} onClick={handleDigitClick}>4</button>
+          <button id="five" className={digitStyle} onClick={handleDigitClick}>5</button>
+          <button id="six" className={digitStyle} onClick={handleDigitClick}>6</button>
           <button id="add" className={controlStyle}>+</button>
           <br/>
-          <button id="one" className={digitStyle}>1</button>
-          <button id="two" className={digitStyle}>2</button>
-          <button id="three" className={digitStyle}>3</button>
+          <button id="one" className={digitStyle} onClick={handleDigitClick}>1</button>
+          <button id="two" className={digitStyle} onClick={handleDigitClick}>2</button>
+          <button id="three" className={digitStyle} onClick={handleDigitClick}>3</button>
           <button id="subtract" className={controlStyle}>-</button>
           <br/>
-          <button id="zero" className={digitStyle}>0</button>
+          <button id="zero" className={digitStyle} onClick={handleDigitClick}>0</button>
           <button id="decimal" className={digitStyle}>.</button>
           <button id="equals" className='w-[120px] h-10 bg-blue-600'>=</button>
         </div>
